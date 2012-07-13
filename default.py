4c6b5a7f6abc4a64389353b4c909b9c3779e9849
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2012-07-13 08:09:11 -0400

    removed défaut progress image
    User can still select a progress images folder, and I'm going to add a
    default of using an XBMC popup

--- default.py
+++ default.py
@@ -221,11 +221,9 @@ class Main:
             self.maxcachesize = int(__addon__.getSetting( "max_cache_size" )) * 1000000
         except:
             self.maxcachesize = 1024 * 1000000
-        self.SHOWPROGRESS = __addon__.getSetting( "show_progress" )
-        self.PROGRESSPATH = __addon__.getSetting( "progress_path" )
-        if len (self.PROGRESSPATH) == 0:
-        	self.PROGRESSPATH = '%s/resources/progress' % __addonpath__
-        	log('set progress path to %s' % self.PROGRESSPATH)
+        if ( len ( __addon__.getSetting( "progress_path" ) ) > 0 ) + ( __addon__.getSetting( "show_progress" ) == "true" ):
+            self.PROGRESSPATH = __addon__.getSetting( "progress_path" )
+            log('set progress path to %s' % self.PROGRESSPATH)
 
 
     def _init_vars( self ):
@@ -294,8 +292,8 @@ class Main:
                 self._get_artistinfo()
         else:
             last_time = 0
-            if self.SHOWPROGRESS == "true":
-	            self.WINDOW.setProperty("ArtistSlideshow", self.PROGRESSPATH)
+            if len ( self.PROGRESSPATH ) > 0:
+                self.WINDOW.setProperty("ArtistSlideshow", self.PROGRESSPATH)
 
         if self.LASTFM == "true":
             lastfmlist = self._get_images('lastfm')
