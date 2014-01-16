a70984860ecea219826c9fc7f16d028fb31c83ab
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2014-01-16 13:18:44 -1000

    fixed crash when trying to log unsupported object

--- resources/common/xlogger.py
+++ resources/common/xlogger.py
@@ -1,23 +1,24 @@
-import xbmc, unicodedata
-
-__log_preamble__ = ''
+import xbmc
 
 #this class creates an object used to log stuff to the xbmc log file
 class Logger():
     def __init__(self, preamble=''):
-        global __log_preamble__
-        __log_preamble__ = preamble
+        self.logpreamble = preamble
 
 
     def _output( self, line, loglevel ):
-        if type(line).__name__=='unicode':
-            line = line.encode('utf-8')
-        xbmc.log("%s %s" % (__log_preamble__, line.__str__()), loglevel)
- 
+        try:
+            xbmc.log("%s %s" % (self.logpreamble, line.__str__()), loglevel)
+        except Exception, e:
+            xbmc.log("%s unable to output logline" % self.logpreamble, loglevel)
+            xbmc.log("%s %s" % (self.logpreamble, e.__str__()), loglevel)
+            
 
     def log( self, loglines, loglevel=xbmc.LOGDEBUG ):
         for line in loglines:
             try:
+                if type(line).__name__=='unicode':
+                    line = line.encode('utf-8')
                 str_line = line.__str__()
             except Exception, e:
                 str_line = ''
