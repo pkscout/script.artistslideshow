f998fdc6bef48b89e795b4498bca01e17312b923
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2014-01-16 13:17:41 -1000

    removed unneeded functions

--- resources/common/fileops.py
+++ resources/common/fileops.py
@@ -1,6 +1,5 @@
-import ntpath, os, socket, urllib, urllib2, xbmc, xbmcvfs
+import ntpath, xbmcvfs
 
-socket.setdefaulttimeout(10)
 
 def checkDir(path):
     if not xbmcvfs.exists(path):
@@ -9,55 +8,6 @@ def checkDir(path):
 def pathLeaf(path):
     path, filename = ntpath.split(path)
     return {"path":path, "filename":filename}
-
-def saveURL( url, filename, *args, **kwargs ):
-    data, log_lines = grabURL( url, *args, **kwargs )
-    if data:
-        success, log_lines2 = writeFile( data, filename )
-        log_lines.extend( log_lines2 )
-        if success:
-            return True, log_lines
-        else:
-            return False, log_lines
-    else:
-        return False, log_lines
-
-def grabURL( url, *args, **kwargs ):
-    log_lines = []
-    req = urllib2.Request( url=url )
-    log_lines.append( 'url is ' + url )
-    for key, value in kwargs.items():
-        req.add_header(key.replace('_', '-'), value)
-    for header, value in req.headers.items():
-        log_lines.append( 'url header %s is %s' % (header, value) )
-    params = ''
-    for arg in args:
-        if params:
-           join = '&'
-        else:
-            join = ''
-        try:
-            params = params + join + urllib.urlencode( arg )
-        except Exception, e:
-            log_lines.append( 'unknown error urlencoding args' )
-            log_lines.append( e )
-    try:
-        if params:
-            log_lines.append( 'the POST params are: ' + params )
-            url_data = urllib2.urlopen( req, params ).read()        
-        else:
-            url_data = urllib2.urlopen( req ).read()
-    except urllib2.URLError, urllib2.HTTPError:
-        log_lines.append( 'site unreachable at ' + url )
-        return '', log_lines
-    except socket.error:
-        log_lines.append( 'timeout error while downloading from ' + url )
-        return '', log_lines
-    except Exception, e:
-        log_lines.append( 'unknown error while downloading from ' + url )
-        log_lines.append( e )
-        return '', log_lines
-    return url_data, log_lines    
 
 def writeFile( data, filename ):
     log_lines = []
@@ -70,11 +20,11 @@ def writeFile( data, filename ):
     except IOError, e:
         log_lines.append( 'unable to write data to ' + filename )
         log_lines.append( e )
-        return (False, log_lines)
+        return False, log_lines
     except Exception, e:
         log_lines.append( 'unknown error while writing data to ' + filename )
         log_lines.append( e )
-        return (False, log_lines)
+        return False, log_lines
     log_lines.append( 'successfuly wrote data to ' + filename )
     return True, log_lines
 
@@ -87,11 +37,12 @@ def readFile( filename ):
             the_file.close()
         except IOError:
             log_lines.append( 'unable to read data from ' + filename )
-            return '', log_lines
+            return log_lines, ''
         except Exception, e:
             log_lines.append( 'unknown error while reading data from ' + filename )
             log_lines.append( e )
-            return '', log_lines
-        return data, log_lines
+            return log_lines, ''
+        return log_lines, data
     else:
-        return '', log_lines
+        log_lines.append( '%s does not exist' % filename )
+        return log_lines, ''
