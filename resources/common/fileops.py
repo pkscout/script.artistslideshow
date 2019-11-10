e7cb90769f9a895b117cce26e5ca863ba3090dd8
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2019-11-10 06:38:32 -1000

    added natural sort and fixed deleteFolder

--- resources/common/fileops.py
+++ resources/common/fileops.py
@@ -1,6 +1,6 @@
 # v.0.5
 
-import shutil, time
+import os, re, shutil, time
 try:
     _range = range
 except NameError:
@@ -15,7 +15,6 @@ try:
     from kodi_six import xbmcvfs
     isXBMC = True
 except:
-    import os
     isXBMC= False
 
 if isXBMC:
@@ -74,12 +73,24 @@ def deleteFolder( src, type='folder' ):
     log_lines = []
     if _exists( src ):
         if type == 'folder':
+            #in Mac OSX the .DS_Store file, if present, will block a folder from being deleted, so delete the file
+            try:
+                _delete( os.path.join( src, '.DS_Store' ) )
+            except IOError:
+                log_lines.append( 'unable to delete .DS_Store file' )
+            except Exception as e:
+                log_lines.append( 'unknown error while attempting to delete .DS_Store file' )
+                log_lines.append( e )
             _action = _rmdir
         else:
             _action = _delete
         try:
             log_lines.append( 'deleting %s %s' % (type, src) )
-            _action( src )
+            if isXBMC:
+                if not _action( src ):
+                    raise IOError( 'unable to delete item' )
+            else:
+                _action( src )                
         except IOError:
             log_lines.append( 'unable to delete %s' % src )
             return False, log_lines
@@ -108,6 +119,18 @@ def moveFile( src, dst ):
         log_lines.append( '%s does not exist' % src)
         success = False
     return success, log_lines + cp_loglines + dl_loglines
+
+
+def atoi( text ):
+    return int(text) if text.isdigit() else text
+
+
+def naturalKeys( text ):
+    '''
+    alist.sort( key=naturalKeys ) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    '''
+    return [ atoi( c ) for c in re.split( r'(\d+)', text ) ]
 
 
 def popenWithTimeout( command, timeout ):
