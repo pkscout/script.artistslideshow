ddd4faa83d0fdd389f5dec37670b98087f73c105
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-01-08 15:51:46 -1000

    separate init from run

--- default.py
+++ default.py
@@ -3,4 +3,5 @@ import resources.lib.artistslideshow as ArtistSlideshow
 
 if ( __name__ == "__main__" ):
     slideshow = ArtistSlideshow.Main()
+    slideshow.run()
 xbmc.log( '[Artist Slideshow] script stopped', xbmc.LOGNOTICE )
